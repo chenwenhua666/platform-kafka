36b1767be49eedba42d1b840a5456638bfbf7725
chenwenhua666 <764471698@qq.com>
2022-04-13 21:30:54 +0800

    修改地址

--- src/main/java/com/oumasoft/platform/kafka/controller/SendMessageController.java
+++ src/main/java/com/oumasoft/platform/kafka/controller/SendMessageController.java
@@ -3,6 +3,7 @@ package com.oumasoft.platform.kafka.controller;
 import com.oumasoft.platform.kafka.service.SendMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ import static com.oumasoft.platform.kafka.constants.KafkaConstant.SUCCESS;
 public class SendMessageController {
 
     private final SendMessageService sendMessageService;
+
+    @GetMapping("sendTest")
+    public String getSendTest(@NotBlank(message = "{required}") String topic, @NotBlank(message = "{required}") String message) {
+        sendMessageService.send(topic, message);
+        return SUCCESS;
+    }
 
     @PostMapping("send")
     public String postSend(@NotBlank(message = "{required}") String topic, @NotBlank(message = "{required}") String message) {
